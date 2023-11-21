a0d1ed6b226cc346173ed53e1bf3d0796a7cad92
fnal4hire <rubenpangestu14@gmail.com>
2023-11-22 00:21:31 +0700

    added prepared statements for register controller

--- HCI/scripts/register.php
+++ HCI/scripts/register.php
@@ -7,8 +7,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $email = $_POST['email'];
 
   // Check if username or email already exists
-  $check_query = "SELECT id FROM users WHERE username = '$username' OR email = '$email'";
-  $check_result = $conn->query($check_query);
+  $check_query = "SELECT id FROM users WHERE username = ? OR email = ?";
+  $stc=$conn->prepare($check_query);
+  $stc->bind_param("ss",$username,$email);
+  $check_result = $conn->execute($stc);
 
   if ($check_result->num_rows > 0) {
     echo "Username or Email already exists";
@@ -17,8 +19,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
     // Query to insert new user
-    $insert_query = "INSERT INTO users (username, password, email) VALUES ('$username', '$hashed_password', '$email')";
-    $success = $conn->query($insert_query);
+    $insert_query = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+    $sti=$conn_>prepare($insert_query);
+    $sti->bind_param ("sss",$username,$hashed_password,$email);
+    $success = $conn->execute($sti);
 
     if ($success) {
       echo "Registration successful";
