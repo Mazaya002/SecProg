0225c3e7bacb4c9669c58313db8dc96b4b548f5d
Mazaya Satrio <90241788+Mazaya002@users.noreply.github.com>
2023-12-31 11:21:30 +0100

    update logout

--- HCI/scripts/logout.php
+++ HCI/scripts/logout.php
@@ -1,13 +1,10 @@
 <?php
 session_start();
-
 // Unset all of the session variables
 $_SESSION = array();
-
 // Destroy the session.
 session_destroy();
-
 // Redirect to index.html
-header("Location: ../index.html");
+header("Location: ../login.html");
 exit;
 ?>
