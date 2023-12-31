643e0c2f70270e7701b7944490db12c53d1919f8
fnal4hire <rubenpangestu14@gmail.com>
2023-12-31 21:41:39 +0700

    added rate limit for login

--- HCI/scripts/login.php
+++ HCI/scripts/login.php
@@ -2,6 +2,27 @@
 include 'db.php';
 
 session_start();
+$login= false;
+function checkattempt($username){
+    $max= 5;
+    $lock_time= 3600;
+    if(isset($_SESSION['login_attempt'][$username])){
+        $attempt= $_SESSION['login_attempt'][$username]['attempt'];
+        $time= $_SESSION['login_attempt'][$username]['time'];
+        $curr_time= time();
+
+        if(($curr_time-$time)<$lock_time && $attempt>=$max){
+            return false;
+        }elseif (($curr_time-$time)>$lock_time){
+            unset ($_SESSION['login_attempt'][$username]);//reset attempt count
+            return true;
+        }else{
+            return true;//percobaan masih dalam batas dan melebihiwaktu
+        }
+    }else{
+        return true; //g ad coba login
+    }
+}
 
 function clean_input($data) {
     $data = trim($data);
@@ -13,6 +34,10 @@ function clean_input($data) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = clean_input($_POST['username']);
     $password = clean_input($_POST['password']);
+    if(!checkattempt($username)){
+        echo "account locked"
+        exit();
+    }
 
     // Prepare a statement to prevent SQL Injection
     $stmt = $conn->prepare("SELECT id, username, password FROM users WHERE username = ?");
@@ -28,20 +53,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['loggedin'] = TRUE;
             $_SESSION['username'] = $row['username'];
             $_SESSION['id'] = $row['id'];
-            
-
+            $login= true;
             echo "Logged in successfully";
         } else {
             // Password is not valid
             echo "Incorrect password";
             echo "DB Hash: " . $row['password'] . "<br>";
             echo "Entered Hash: " . password_hash($password, PASSWORD_DEFAULT) . "<br>";
+            $login= false
         }
     } else {
         // Username does not exist
         echo "Incorrect username";
+        $login= false;
     }
-
+    if($login=== false){
+        if (isset($_SESSION['login_attempt'][$username])){
+            $_SESSION['login_attempt'][$username]=[
+                'attempt'=>1
+                'time'=>time()
+            ];
+        }else{
+            $_SESSION['login_attempt'][$username]['attempt']++;
+        }
+    }
     $stmt->close();
     $conn->close();
 }
