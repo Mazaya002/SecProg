8b6c870f568c0f5f5a6f46f230361bd960932205
fnal4hire <rubenpangestu14@gmail.com>
2024-01-01 13:22:51 +0700

    make the store to load dynamically

---
+++ HCI/scripts/store.php
@@ -0,0 +1,29 @@
+<?php
+    include 'db.php';
+    $load= $conn->prepare('SELECT image, title, price, rating FROM store');
+    $load->execute();
+    $result= $load->get_result();
+    if($result->num_rows>0){
+        while ($row= $result->fetch_assoc()){
+            echo "<div class='card'>";
+            echo "<img src='" . htmlspecialchars($row["image"]) . "' alt='" . htmlspecialchars($row["title"]) . "'>";
+            echo "<h3>" . htmlspecialchars($row["title"]) . "</h3>";
+            echo "<p>Price: $" . htmlspecialchars($row["price"]) . "</p>";
+            
+            // display star based on rating val
+            $stars = $row["rating"];
+            echo "<div class='stars'>";
+            for ($i = 0; $i < $stars; $i++) {
+                echo "<i class='fa fa-star'></i>"; //load rating star
+            }
+            echo "</div>";
+            
+            echo "<button id='addToCart'>Add to Cart</button>";
+            echo "</div>";
+        }
+    } else {
+        echo "nothing to see here atm. Will add smtg in the future";
+    }
+    $load->close();
+    $conn->close();
+?>
